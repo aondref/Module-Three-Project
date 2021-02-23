3fcb41e2b06389fe3ed53045f6fbefcf121bbeaa
aondref <aondref@gmail.com>
2021-02-23 10:03:46 -0500

    Created the income table

--- server/src/main/java/com/example/server/model/Income.java
+++ server/src/main/java/com/example/server/model/Income.java
@@ -1,4 +1,16 @@
 package com.example.server.model;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Data
+@Table(name="income")
 public class Income {
+
+    @Id
+    private Long id;
+    private String Income;
+    private Double amount;
 }
