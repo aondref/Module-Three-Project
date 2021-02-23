ce00ed2313d3a552c15c8176c0ded9482d42ba42
aondref <aondref@gmail.com>
2021-02-23 10:00:15 -0500

    Created the expense table

--- server/src/main/java/com/example/server/model/Expense.java
+++ server/src/main/java/com/example/server/model/Expense.java
@@ -1,4 +1,16 @@
 package com.example.server.model;
+import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Data
+@Table(name="expense")
 public class Expense {
+
+    @Id
+    private Long id;
+    private String expenses;
+    private Double amount;
 }
